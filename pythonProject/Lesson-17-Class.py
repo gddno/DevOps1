04e19f674b12a2cedac6be150586807063e91fe0
Zhuravlev Dmitrii <gddno@inbox.ru>
2022-07-23 01:43:49 +0300

    add class part #1

--- pythonProject/Lesson-17-Class.py
+++ pythonProject/Lesson-17-Class.py
@@ -1,33 +1,4 @@
-
-
-from pydoc import describe
-
-
-class Hero():
-    """Class to creat Hero for our Game"""
-
-    def __init__(self, name, level, race):
-        """Initiate our Hero"""
-        self.name = name
-        self.level = level
-        self.race = race
-        self.health = 100
-
-    def show_hero(self):
-        """Print all aprametrs of this Hero"""
-        discription = (self.name + " " + "Level is: " + str(self.level) + " Race is: " + self.race + " Health is: " + str(self.health)).title()
-        print(discription)
-
-    def level_up(self):
-        """Upgrade Level of Hero"""
-        self.level += 1
-    
-    def move(self):
-        """Start moving Hero"""
-        print("Hero " + self.name + " start moving...")
-    def harm(self):
-        """Harm to my Hero"""
-        self.health -= 1
+from Hero import *
 
 myhero_1 = Hero("Vurdalak", 5, "Orc") 
 myhero_2 = Hero("Alexandr", 4, "Human")
@@ -37,4 +8,7 @@ myhero_2.move()
 myhero_1.move()
 myhero_2.level_up()
 myhero_2.show_hero()
+myhero_2.show_hero()
+myhero_2.set_health(53)
+myhero_2.harm()
 myhero_2.show_hero()
